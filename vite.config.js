c4f5c7c566e5e6023983051e22f58251401b7a99
xsf0105 <allan91coder@gmail.com>
2023-06-09 11:33:18 +0800

    patch: update

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import FullReload from 'vite-plugin-full-reload'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    FullReload(['src/**/*.css'])
-  ],
-});
