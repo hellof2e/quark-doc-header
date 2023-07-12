4d97b306ad4161718e6b83fc1933b6b8707c5680
xsf0105 <allan91coder@gmail.com>
2023-07-12 10:42:12 +0800

    feat: update quarkc

---
+++ lib/types/dark-mode.d.ts
@@ -0,0 +1,8 @@
+import { QuarkElement } from "quarkc";
+export default class DarkMode extends QuarkElement {
+    constructor();
+    componentDidMount(): void;
+    turnDark: () => void;
+    turnLight: () => void;
+    render(): any;
+}
